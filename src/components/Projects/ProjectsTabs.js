22511badb0d1c51df9e3d2dc1f5c4567ad3aa037
Louis Grasset <louisgrasset@protonmail.com>
2020-12-30 00:02:28 +0100

    fix: add tablist role

--- src/components/Projects/ProjectsTabs.js
+++ src/components/Projects/ProjectsTabs.js
@@ -4,7 +4,7 @@ import ProjectsTab from './ProjectsTab';
 const ProjectsTabs = ({ projects, projectSelected, setProject }) => {
     return (
         <div className="max-w-full p-4 overflow-x-auto lg:max-h-projectslist xl:flex-row lg:overflow-x-hidden lg:overflow-y-auto ">
-            <div className="flex flex-nowrap lg:flex-col space-x-1.5 lg:space-x-0 lg:space-y-1.5">
+            <div role="tablist" className="flex flex-nowrap lg:flex-col space-x-1.5 lg:space-x-0 lg:space-y-1.5">
                 {projects.map((project, key) => (
                     <ProjectsTab
                         key={key}
