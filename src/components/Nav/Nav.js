21fbb4da84fa0d50fef2b1a9ecfda4ca418a9183
Louis Grasset <louisgrasset@protonmail.com>
2021-01-01 20:50:56 +0100

    fix: add profile alt text and retina support

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -26,7 +26,7 @@ const Nav = ({ refs }) => {
         <nav className={(scrolling ? " border-gray-200 xl:border-transparent  xl:bg-transparent" : "") + " fixed top-0 flex justify-between items-center w-full p-3 z-50 px-5  md:px-10 xl:px-20 border-b border-transparent transition-all bg-transparent"} >
             <div style={{ zIndex: '-1' }} className={(scrolling ? "show" : "") + " origin-top absolute left-0 w-full h-full bg-white opacity-0"} ></div>
 
-            <img src={require('../../images/profile.jpg')} className={(!scrolling ? "scale-0" : "scale-1 xl:scale-0") + " transition-all bg-transparent transform w-6 h-6 rounded-full shadow-md"} />
+            <img src={require('../../images/profile.jpg')} alt="Louis Grasset" className={(!scrolling ? "scale-0" : "scale-1 xl:scale-0") + " transition-all bg-transparent transform w-6 h-6 rounded-full shadow-md"} />
             <SocialAside hidden={scrollingOnFooter} />
 
         </nav >
