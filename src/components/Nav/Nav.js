98fe96aba0c8cbc1fdc09825d4f6b4f3aec7afb3
Louis Grasset <louisgrasset@protonmail.com>
2021-01-01 17:38:32 +0100

    fix(nav): remove border on xl

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -18,7 +18,7 @@ const Nav = () => {
 
 
     return (
-        <nav className={(scrolling ? "bg-white border xl:bg-transparent" : "") + " fixed top-0 flex justify-between items-center w-full p-3 z-50 px-5  md:px-10 xl:px-20  border-gray-200"} >
+        <nav className={(scrolling ? "bg-white border-gray-200 border xl:border-transparent xl:bg-transparent" : "") + " fixed top-0 flex justify-between items-center w-full p-3 z-50 px-5  md:px-10 xl:px-20"} >
             <img src={require('../../images/profile.jpg')} className={(!scrolling ? "scale-0" : "scale-1 xl:scale-0") + " transition-all bg-transparent transform w-6 h-6 rounded-full shadow-md"} />
             <SocialAside />
         </nav >
