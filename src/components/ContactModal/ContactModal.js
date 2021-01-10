fa515dc24079ab76e6da1f0e368b2bc22afaf9a8
Louis Grasset <louisgrasset@protonmail.com>
2021-01-10 18:27:48 +0100

    fix(responsive): put contactmodal in front of nav

--- src/components/ContactModal/ContactModal.js
+++ src/components/ContactModal/ContactModal.js
@@ -5,9 +5,9 @@ import iconClose from '../../images/icons/close.svg';
 const ContactModal = (props) => {
     const closeModal = () => props.setContactModalActive(false);
     return (
-        <div className={(props.show ? "flex" : "hidden") + " fixed top-0 left-0 items-center justify-center w-full h-screen backdrop-blur-5 z-20"}>
+        <div className={(props.show ? "flex" : "hidden") + " items-start fixed top-0 left-0 md:items-center justify-center w-full h-screen backdrop-blur-5 z-50"}>
             <div onClick={() => closeModal()} role="button" aria-label="Fermer" tabIndex="0" className="absolute w-full h-full bg-gray-900 cursor-pointer opacity-20" style={{ zIndex: -1 }}></div>
-            <div className="container relative grid max-w-xl max-h-screen gap-8 p-10 overflow-y-auto bg-white shadow-2xl overscroll-contain xl:gap-0 xl:max-w-3xl rounded-xl xl:grid-cols-2">
+            <div className="container relative grid max-w-xl max-h-screen gap-8 p-10 mt-5 overflow-y-auto bg-white shadow-2xl md:mt-0 overscroll-contain xl:gap-0 xl:max-w-3xl rounded-xl xl:grid-cols-2">
                 <button className="absolute w-8 h-8 top-4 right-4" aria-label="Fermer" onClick={() => closeModal()}>
                     <img src={iconClose} alt="Croix" />
                 </button>
@@ -54,7 +54,9 @@ const ContactModal = (props) => {
                         <textarea name="message" id="message" className="block w-full px-3 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent sm:text-sm h-30"></textarea>
                     </div>
                     <div>
-                        <input required className="block h-12 px-6 font-medium text-white uppercase bg-gray-900 rounded-md cursor-pointer focus:outline-none focus:ring-4 focus:ring-pink-500 focus:border-pink-500" type="submit" value="Envoyer" />
+                        <button className="block h-12 px-6 font-medium text-white uppercase bg-gray-900 rounded-md cursor-pointer group focus:outline-none focus:ring-4 focus:ring-pink-500 focus:border-pink-500">
+                            Envoyer
+                        </button>
                     </div>
                 </form>
             </div>
