6adf46372e72ffc9d05c8d8d33f2510d72f7f3c3
Louis Grasset <louisgrasset@protonmail.com>
2021-04-09 08:55:47 +0200

    feat: optimize animations and reorder scss

--- src/components/BeachBall/BeachBall.js
+++ src/components/BeachBall/BeachBall.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export const BeachBall = () => {
     return (
-        <div className={"absolute top-0 right-0 overflow-hidden filter-blur-70 dark:opacity-20"} style={{ animation: 'spin 12s ease-in-out alternate infinite', zIndex: '-1' }}>
+        <div id="beachball" className={"absolute top-0 right-0 overflow-hidden filter-blur-70 dark:opacity-20"} style={{ animation: 'spin 12s ease-in-out alternate infinite', zIndex: '-1' }}>
             <div className="relative grid grid-cols-2 origin-center transform -rotate-45 dark:filter-grayscale-30">
                 <div className="top-0 left-0 bg-yellow-400 rounded-full rounded-br-none w-60 h-60"></div>
                 <div className="top-0 right-0 bg-pink-400 rounded-full rounded-bl-none w-60 h-60"></div>
