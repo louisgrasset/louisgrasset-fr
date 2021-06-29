e0b245004e04857ae2a3aac94d65b0b994da1f01
Louis Grasset <louisgrasset@protonmail.com>
2021-06-29 12:57:23 +0200

    fix: safari image rendering

--- src/components/Studies/Studies.js
+++ src/components/Studies/Studies.js
@@ -9,7 +9,7 @@ export const Studies = () => {
                     console.log(diploma.institution.slug);
                     return (
                         <div key={key} className="flex flex-col items-start gap-12 lg:items-center lg:flex-row gap-y-4">
-                            <img src={require(`../../images/studies/${diploma.institution.slug}.svg`).default} alt={diploma.institution.name} width="100" height="50" className="w-24 lg:w-30 dark:filter-companies" />
+                            <img src={require(`../../images/studies/${diploma.institution.slug}.svg`).default} alt={diploma.institution.name} width="100" height="50" className="w-24 h-12 lg:w-30 dark:filter-companies" />
                             <div className="dark:text-white">
                                 <h3 className="text-xl font-bold">{diploma.label}</h3>
                                 <p>{diploma.institution.name}</p>
