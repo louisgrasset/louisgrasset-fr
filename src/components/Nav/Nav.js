6b894a2031736930d7b66ba5eb53657c4b87f0ce
Louis Grasset <louisgrasset@protonmail.com>
2021-06-29 08:27:17 +0200

    fix: use .default for require

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -27,7 +27,7 @@ export const Nav = ({ refs }) => {
         <nav className={"fixed top-0 left-0 w-full py-3 z-40 px-5  md:px-10 xl:px-20"}>
             <div style={{ zIndex: '-1' }} className={(scrolling ? "show  border-gray-200 dark:border-gray-800 " : "") + "origin-top  absolute left-0 w-full h-full  bg-white dark:bg-gray-900  opacity-0 -mt-3  xl:hidden  border-b border-transparent xl:border-transparent transition-all"} ></div>
             <div style={{ gridTemplateColumns: `24px 1fr ${socials.length * 36}px` }} className="grid items-center grid-flow-col">
-                <img src={require('../../images/profile.jpg')} alt="Louis Grasset" className={(!scrolling ? "scale-0" : "scale-1 xl:hidden") + " duration-200 transition-all bg-transparent transform w-6 h-6 rounded-full shadow-md"} />
+                <img src={require('../../images/profile.jpg').default} alt="Louis Grasset" className={(!scrolling ? "scale-0" : "scale-1 xl:hidden") + " duration-200 transition-all bg-transparent transform w-6 h-6 rounded-full shadow-md"} />
                 <div className="h-6 overflow-x-hidden font-semibold xl:invisible" >
                     <span className={(scrolling ? "opacity-100 translate-x-3 " : "-translate-x-24 ") + "opacity-0 block transform transition-all ease-out dark:text-white"}>Louis Grasset</span>
                 </div>
