be221f5f25a6d68fccabc3c53d1904de99a29721
Louis Grasset <louisgrasset@users.noreply.github.com>
2021-09-10 02:52:31 +0200

    feat: add skills (#3)

---
+++ src/data/skills.js
@@ -0,0 +1,71 @@
+import javascript from "../images/skills/javascript.svg";
+import javascriptDark from "../images/skills/javascriptDark.svg";
+import typescript from "../images/skills/typescript.svg";
+import reactjs from "../images/skills/reactjs.svg";
+import html from "../images/skills/html.svg";
+import htmlDark from "../images/skills/htmlDark.svg";
+import css from "../images/skills/css.svg";
+import cssDark from "../images/skills/cssDark.svg";
+import sass from "../images/skills/sass.svg";
+import laravel from "../images/skills/laravel.svg";
+import npm from "../images/skills/npm.svg";
+import jest from "../images/skills/jest.svg";
+import docker from "../images/skills/docker.svg";
+
+const skills = [
+    {
+        slug: 'javascript',
+        name: 'JavaScript',
+        icon: javascript,
+        iconDark: javascriptDark
+    },
+    {
+        slug: 'typescript',
+        name: 'TypeScript',
+        icon: typescript,
+    },
+    {
+        slug: 'reactjs',
+        name: 'ReactJs',
+        icon: reactjs,
+    },
+    {
+        slug: 'sass',
+        name: 'SASS',
+        icon: sass,
+    },
+    {
+        slug: 'jest',
+        name: 'Jest',
+        icon: jest,
+    },
+    {
+        slug: 'laravel',
+        name: 'Laravel',
+        icon: laravel,
+    },
+    {
+        slug: 'docker',
+        name: 'Docker',
+        icon: docker,
+    },
+    {
+        slug: 'npm',
+        name: 'NPM',
+        icon: npm,
+    },
+    {
+        slug: 'html',
+        name: 'HTML',
+        icon: html,
+        iconDark: htmlDark
+    },
+    {
+        slug: 'css',
+        name: 'CSS',
+        icon: css,
+        iconDark: cssDark
+    },
+]
+
+export default skills;
