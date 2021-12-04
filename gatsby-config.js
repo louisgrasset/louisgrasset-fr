f9edb0fa4cb24b0ad0e2594d4492a335fd7d2532
Louis Grasset <louisgrasset@protonmail.com>
2021-12-04 10:02:02 +0100

    feat: add gatsby-plugin-typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-typescript",
     {
       resolve: "gatsby-source-filesystem",
       options: {
