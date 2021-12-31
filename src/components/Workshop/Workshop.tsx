a2d77a6287cf6d8884b2ae15a56ebefd9b4ac7d2
Louis Grasset <louisgrasset@users.noreply.github.com>
2021-12-31 09:49:24 +0100

    feat: use thumb on small screens (#16)

--- src/components/Workshop/Workshop.tsx
+++ src/components/Workshop/Workshop.tsx
@@ -10,6 +10,7 @@ export const Workshop = () => {
                     <a key={key} className="" href={project.url} target="_blank" rel="noreferrer">
                         <article className="relative p-4 pb-2 bg-gray-100 rounded-md dark:bg-gray-800">
                         <picture>
+                            <source className="object-fit aspect-video rounded-md h-full w-full" srcSet={`/images/workshop/${project.slug}/${project.slug}-1.thumb.webp`} media="(max-width: 420px)"/>
                             <source className="object-fit aspect-video rounded-md h-full w-full" srcSet={`/images/workshop/${project.slug}/${project.slug}-1.webp`} media="(max-width: 768px)"/>
                             <img className="object-fit aspect-video rounded-md h-full w-full" src={`/images/workshop/${project.slug}/${project.slug}-1.thumb.webp`} alt={project.name} />
                         </picture>
