a6b6dfc98196687628c61aad24670a780053d89f
Louis Grasset <louisgrasset@protonmail.com>
2021-12-31 08:38:03 +0100

    feat: use `picture` tag inside workshop

--- src/components/Workshop/Workshop.tsx
+++ src/components/Workshop/Workshop.tsx
@@ -9,7 +9,10 @@ export const Workshop = () => {
                 workshop.map((project, key) => (
                     <a key={key} className="" href={project.url} target="_blank" rel="noreferrer">
                         <article className="relative p-4 pb-2 bg-gray-100 rounded-md dark:bg-gray-800">
-                            <div className="bg-cover rounded-md" style={{ paddingBottom: '56.25%', backgroundImage: `url('/images/workshop/${project.slug}/${project.slug}-1.webp')` }}></div>
+                        <picture>
+                            <source className="object-fit aspect-video rounded-md h-full w-full" srcSet={`/images/workshop/${project.slug}/${project.slug}-1.webp`} media="(max-width: 768px)"/>
+                            <img className="object-fit aspect-video rounded-md h-full w-full" src={`/images/workshop/${project.slug}/${project.slug}-1.thumb.webp`} alt={project.name} />
+                        </picture>
                             <h3 className="mt-2 text-lg font-bold dark:text-white">{project.name}</h3>
                             <span className="absolute text-gray-500 bottom-2 right-4">{project.year}</span>
                         </article>
