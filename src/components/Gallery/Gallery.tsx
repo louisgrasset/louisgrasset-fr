5caccd34b6e310fdb87db192927ddb8bc563428b
Louis Grasset <louisgrasset@protonmail.com>
2022-01-02 15:16:14 +0100

    fix: use performance hooks

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo} from 'react';
 import { Project } from '../../types';
 import { GalleryButton } from './GalleryButton';
 import { GalleryImage } from './GalleryImage';
+import { useCallback } from 'react';
 
 interface GalleryProps {
     /** The Project to handle. */
@@ -21,22 +22,25 @@ export const Gallery = ({ project, projectSelected }: GalleryProps) => {
         setImage({ counter: 1, max: 1 });
     }, [projectSelected]);
 
-    const nextImage = () => {
+    const nextImage = useCallback(() => {
         (image.counter + 1 <= project.images)
             ? setImage({ ...image, counter: image.counter + 1, max: image.counter + 1 })
             : setImage({ ...image, counter: 1 });
-    };
+    }, [image, project.images, setImage]);
 
-    const previousImage = () => {
+    const previousImage = useCallback(() => {
         (image.counter - 1 > 0)
             ? setImage({ ...image, counter: image.counter - 1 })
             : setImage({ ...image, counter: project.images });
-    };
+    }, [image, project.images, setImage]);
 
-    let images = [];
-    for (let key = 1; key <= image.max; key++) {
-        images.push(<GalleryImage key={key} counter={key} project={project} />);
-    }
+    const images = useMemo(()=> {
+        let i = [];
+        for (let key = 1; key <= image.max; key++) {
+            i.push(<GalleryImage key={key} counter={key} project={project} />);
+        }
+        return i;
+    }, [image, project]);
 
     return (
         <div className="relative h-full overflow-hidden lg:rounded-r-xl">
